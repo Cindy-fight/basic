c68763695ac4e3a5c019b9f3a3f8108053e97691
Cindy-fight <ting.wang@maimob.cn>
2017-06-23 18:31:54 +0800

    测试

--- controllers/CountryController.php
+++ controllers/CountryController.php
@@ -134,4 +134,11 @@ class CountryController extends Controller
     
     	return $this->redirect(['index']);
     }
+    
+    public function actionDeletedss($id)
+    {
+    	$this->findModel($id)->delete();
+    
+    	return $this->redirect(['index']);
+    }
 }
