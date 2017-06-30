3f3ed1e68adab3c9a2a899bcd0811e7cbe87dac2
Cindy-fight <ting.wang@maimob.cn>
2017-06-30 16:05:57 +0800

    发送短信学习

---
+++ views/test/email.php
@@ -0,0 +1,8 @@
+<?php
+ use yii\helpers\Html;
+ use yii\helpers\Url;
+ ?>
+ 
+ <h2>This message allows you to visit our site home page by one click</h2>
+ <?= Html::a('Go to home page', Url::home('http')) ?>
+ 
